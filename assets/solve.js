85c35382b71e18ed9531cab0ea556bd4b03acd8b
0xecho <0xecho@proto-code.com>
2021-10-25 11:56:29 +0300

    Adds basic functions and solver

---
+++ assets/solve.js
@@ -0,0 +1,174 @@
+function _solve() {
+    let count = 0;
+
+    let max_depth = 30;
+
+    let initial_state = main_state.asArray();
+    FLASK_ID = initial_state.length;
+    
+    let possibles = [
+        [[], initial_state, 0],
+    ];
+    console.log(JSON.stringify(initial_state))
+
+
+    while (possibles.length) {
+        let [moves, current_state, depth] = possibles.shift();
+        if (is_solved(current_state)) {
+            console.log("SOLVED WITH", JSON.stringify(moves));
+            return moves
+        }
+        if (depth > max_depth) {
+            continue;
+        }
+
+        for (let fidx = 0; fidx < current_state.length; fidx++) {
+            let [flask_id, data] = current_state[fidx];
+            let top_color = null;
+            let top_count = 0;
+            for (let idx = 0; idx < data.length; idx++) {
+                let color = data[idx]
+                if (color == "#ffffff") continue;
+                if (top_color == null) {
+                    top_color = color;
+                    top_count += 1;
+                } else {
+                    if (top_color == color) {
+                        top_count += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            for (let ofidx = 0; ofidx < current_state.length; ofidx++) {
+                if (ofidx == fidx) continue;
+                let [target_flask_id, target_data] = current_state[ofidx];
+                let target_top_color = null;
+                for (let idx = 0; idx < target_data.length; idx++) {
+                    let color = target_data[idx];
+                    if (color == "#ffffff") continue;
+                    target_top_color = color; break;
+                }
+                let remaining = remaining_size(target_data);
+                // console.log(data);
+                // console.log(target_data);
+                // console.log(top_color, target_top_color);
+                // console.log();
+                if (((top_color == target_top_color) || (target_top_color == null)) && top_count <= remaining) {
+                    // CREATE NEW POSSIBLE STATE
+                    let new_move = [flask_id, target_flask_id];
+                    let new_state = [];
+                    // console.log(new_move);
+                    for (let i = 0; i < FLASK_ID; i++) { //loop 
+                        if (i == ofidx) {
+                            let current_flask_state = [i];
+                            // console.log("FILLED", target_data, top_color, top_count, remaining)
+                            current_flask_state.push(fill(target_data.slice(), top_color, top_count, remaining));
+                            // console.log("AFTER", current_flask_state);
+                            new_state.push(
+                                current_flask_state
+                            );
+                        } else if (i == fidx) {
+                            let current_flask_state = [i];
+                            // console.log("UNFILLED", data, top_count)
+                            current_flask_state.push(unfill(data.slice(), top_count));
+                            // console.log("AFTER", current_flask_state);
+                            new_state.push(
+                                current_flask_state
+                            );
+                        } else {
+                            new_state.push(
+                                [i, current_state[i][1].slice()]
+                            );
+                        }
+                    }
+                    count ++;
+                    if(count%100==0)console.log('.');
+                    let new_moves = moves.slice();
+                    new_moves.push(new_move);
+                    possibles.push([new_moves, new_state, depth + 1]);
+                    // console.log("NEWS", JSON.stringify([new_moves, new_state, depth+1]));
+                }
+            }
+        }
+    }
+    return null;
+}
+
+function is_valid(data) {
+    let valid = true;
+    let white_found = false;
+    for (let i = data.length - 1; i >= 0; i--) {
+        let item = data[i];
+        if (white_found) {
+            if (item != "#ffffff") {
+                valid = false;
+            }
+        } else {
+            white_found = (item == "#ffffff");
+        }
+    }
+    return valid;
+}
+
+function remaining_size(data) {
+    if (is_valid(data)) {
+        let count = 0;
+        for (let i = data.length - 1; i >= 0; i--) {
+            let item = data[i];
+            if (item == "#ffffff") {
+                count += 1;
+            }
+        }
+        return count;
+    }
+    return 0;
+}
+
+function fill(data, color, size, remaining) {
+    let start_idx = remaining - size;
+    // console.log("start_idx", start_idx);
+    for (let idx = start_idx; idx < remaining; idx++) {
+        data[idx] = color;
+    }
+    return data;
+}
+
+function unfill(data, size) {
+    let remaining = size;
+    for (let idx = 0; idx < data.length; idx++) {
+        if (data[idx] != "#ffffff") {
+            remaining -= 1;
+        }
+        if (remaining >= 0) data[idx] = "#ffffff";
+
+    }
+    return data;
+}
+
+function get_copy(state) {
+    let new_state = [];
+    for (let i = 0; i < state.length; i++) {
+        new_state.push(state[i].slice());
+    }
+    return new_state;
+}
+
+function is_solved(data) {
+    for (let idx = 0; idx < data.length; idx++) {
+        let [flask_id, flask_data] = data[idx];
+        // console.log("D2",data[idx]);
+        // console.log("D",flask_data);
+        let flask_color = null;
+        for (let ridx = 0; ridx < flask_data.length; ridx++) {
+            if (flask_data[ridx] == "#ffffff") continue;
+            if (flask_color == null) {
+                flask_color = flask_data[ridx];
+                continue
+            }
+            else if (flask_color == flask_data[ridx]) continue;
+            return false
+        }
+    }
+    return true;
+}
--- assets/view.js
+++ assets/view.js
@@ -1,1 +1,222 @@
-// const flask_template = "";
+class State {
+    FLASK_ID = 0
+    FLASKS_DATA = {
+
+    }
+
+    addFlask(height){
+        let newFlask = new Flask(this.FLASK_ID, height, this);
+        newFlask.render_html();
+        this.FLASKS_DATA[this.FLASK_ID] = newFlask;
+        this.FLASK_ID += 1;
+        return newFlask
+    }
+
+    addFlaskFromPrompt(){
+        // let userHeight = parseInt(prompt("Enter the new height"));
+        let userHeight = 4;
+        if (!isNaN(userHeight)){
+            this.addFlask(userHeight)
+        }
+    }
+
+    removeFlask(flaskID){
+        let flask = this.FLASKS_DATA[flaskID]
+        if(flask != null) {
+            flask.remove()
+            delete this.FLASKS_DATA[flaskID];
+        }
+    }
+
+    asArray(){
+        let new_flasks_array = [];
+        Object.keys(this.FLASKS_DATA).forEach((elem) => {
+            new_flasks_array.push([elem, this.FLASKS_DATA[elem].data]);
+        })
+        return new_flasks_array;
+    }
+    
+    fromArray(array){
+        Object.keys(this.FLASKS_DATA).forEach((elem) => {
+            this.removeFlask(elem)
+        })
+        array.forEach(elem=>{
+            let newFlask = this.addFlask(elem[1].length);
+            newFlask.data = elem[1];
+            newFlask.render_html()
+        })
+    }
+
+    exportToLocalStorage(){
+        
+    }
+
+    importFromLocalStorage(){
+
+    }
+
+    setHead(text){
+        document.getElementById("solution").innerText = text
+    }
+
+    solve(){
+        let solution = _solve()
+
+        if (solution == null) {
+            this.setHead("INVALID OR UNSOLVABLE STATE")
+        }
+        else if (solution.length== 0) { 
+            this.setHead("ALREADY IN SOLVED STATE")
+        }else {
+            let opt = "";
+            solution.forEach(elem=>{
+                opt += `Flask {${elem[0]}} to Flask {${elem[1]}}, `
+            })
+            this.setHead(opt)
+        }
+
+    }
+
+}
+
+class Flask {
+
+    constructor(FLASK_ID, height, state) {
+        this.height = height;
+        this.state = state
+        this.data = [];
+        this.FLASK_ID = FLASK_ID;
+        FLASK_ID += 1;
+        for (let i = 0; i < this.height; i++) {
+            this.data.push("#ffffff");
+        }
+    }
+
+    remove () {
+        let main_div = document.getElementById(`flask_${this.FLASK_ID}`);
+        main_div.textContent = "";
+    }
+
+    render_html() {
+        let app = document.getElementById("app");
+
+        let main_div = document.getElementById(`flask_${this.FLASK_ID}`);
+        // If flask_div not found, create it
+        if (!main_div) {
+            main_div = document.createElement("div");
+            main_div.id = `flask_${this.FLASK_ID}`;
+            main_div.style.display = "inline-block";
+            main_div.style.marginLeft = "100px"
+            app.appendChild(main_div)
+        }
+        else {
+            main_div.textContent = "";
+        }
+
+        let head = document.createElement("h3")
+        head.innerText = this.FLASK_ID
+        main_div.appendChild(head)
+
+        for (let i = 0; i < this.height; i++) {
+            let square = document.createElement("div");
+            square.id = `square_${this.FLASK_ID}_${i}`;
+            square.classList.add("square");
+            square.style.backgroundColor = this.data[i];
+            let color_input = document.createElement("input");
+            color_input.type = "color";
+            color_input.id = `square_${this.FLASK_ID}_${i}_input`;
+            color_input.setAttribute("data-flask_id", this.FLASK_ID);
+            color_input.setAttribute("data-square_id", i);
+            color_input.hidden = true;
+            color_input.value = this.data[i];
+            color_input.onchange = (event) => {
+                // console.log(this)
+                let target = event.target;
+                let color = target.value;
+                let flask_id = target.getAttribute("data-flask_id");
+                let square_id = target.getAttribute("data-square_id");
+                let square = document.getElementById(`square_${flask_id}_${square_id}`);
+                this.data[i] = color;
+                square.style.backgroundColor = color;
+                this.render_html();
+            }
+            square.onclick = () => {
+                document.getElementById(color_input.id).click()
+            };
+            main_div.appendChild(square)
+            square.appendChild(color_input)
+            if (i == this.height - 1) {
+                square.classList.add("rounded");
+            }
+        }
+        let closeBtn = document.createElement("button")
+        closeBtn.innerText = "X"
+        closeBtn.onclick = (evt)=>{
+            this.state.removeFlask(this.FLASK_ID)
+        }
+        main_div.appendChild(closeBtn)
+    }
+
+    is_valid() {
+        let valid = true;
+        let white_found = false;
+        for (let i = this.height - 1; i >= 0; i--) {
+            let item = this.data[i];
+            if (white_found) {
+                if (item != "#ffffff") {
+                    valid = false;
+                }
+            } else {
+                white_found = (item == "#ffffff");
+            }
+        }
+        return valid;
+    }
+
+    remaining_size() {
+        if (this.is_valid()) {
+            let count = 0;
+            for (let i = this.height - 1; i >= 0; i--) {
+                let item = this.data[i];
+                if (item == "#ffffff") {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+        return 0;
+    }
+
+    get_top() {
+        for (let idx = 0; idx < this.height; idx++) {
+            if (this.data[idx] != "#ffffff") {
+                return this.data[idx];
+            }
+        }
+        return null;
+    }
+
+    fill(color, size) {
+        let remaining = this.remaining_size();
+        if (size > remaining) {
+            return;
+        }
+        let top = this.get_top();
+        // console.log("top", top);
+        if (top == null || top == color) {
+
+            let start_idx = remaining - size;
+            for (let idx = start_idx; idx < remaining; idx++) {
+                this.data[idx] = color;
+            }
+            this.render_html()
+        }
+    }
+
+    clone() {
+        let new_flask = new Flask(0)
+        FLASK_ID -= 1;
+        new_flask.data = this.data.slice()
+        return new_flask;
+    }
+}
